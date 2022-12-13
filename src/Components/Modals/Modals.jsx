2ca7aeb7404dce59b2ec78493866a5603ac73031
Richardd82 <richardd82@gmail.com>
2022-12-13 03:19:12 -0600

    Shadow to container

--- src/Components/Modals/Modals.jsx
+++ src/Components/Modals/Modals.jsx
@@ -4,24 +4,36 @@ import Register from "../Register/Register";
 import Modal from "./Modal/Modal";
 import { useModal } from "./useModal/useModal";
 
-
 const Modals = () => {
-    const [isOpenModalLogin, openModalLogin, closeModalLogin] = useModal(false)
-    const [isOpenModalRegister, openModalRegister, closeModalRegister] = useModal(false)
-    const [login, setLogin] = useState(true)
-	return (
-		<div>
-			<h1>Modals</h1>
-            <button className="btnsHome" onClick={openModalLogin}>Login</button>
-            <Modal isOpen={isOpenModalLogin} closeModal={closeModalLogin}>
-                {login ? <Login setLogin={setLogin}/> : <Register setLogin={setLogin} />}
-            </Modal>
-            <button className="btnsHome" onClick={openModalRegister}>Register</button>
-            <Modal isOpen={isOpenModalRegister} closeModal={closeModalRegister}>
-            {login ? <Login setLogin={setLogin}/> : <Register setLogin={setLogin} />}
-            </Modal>
-		</div>
-	);
+  const [isOpenModalLogin, openModalLogin, closeModalLogin] = useModal(false);
+  const [isOpenModalRegister, openModalRegister, closeModalRegister] =
+    useModal(false);
+  const [login, setLogin] = useState(true);
+  return (
+    <div>
+      <h1>Modals</h1>
+      <button className="btnsHome" onClick={openModalLogin}>
+        Login
+      </button>
+      <Modal isOpen={isOpenModalLogin} closeModal={closeModalLogin}>
+        {login ? (
+          <Login setLogin={setLogin} />
+        ) : (
+          <Register setLogin={setLogin} />
+        )}
+      </Modal>
+      <button className="btnsHome" onClick={openModalRegister}>
+        Register
+      </button>
+      <Modal isOpen={isOpenModalRegister} closeModal={closeModalRegister}>
+        {login ? (
+          <Login setLogin={setLogin} />
+        ) : (
+          <Register setLogin={setLogin} />
+        )}
+      </Modal>
+    </div>
+  );
 };
 
 export default Modals;
